d6a1b5824c16b9fd78b8ba39d21886259a6fd10f
Chukwudobe Micah <micah@refine.agency>
2024-08-03 18:54:16 +0100

    cchore: created solpages bot

--- pages/api/bot.js
+++ pages/api/bot.js
@@ -1,24 +1,25 @@
-// require("dotenv").config();
 const { Telegraf } = require("telegraf");
-const bot = new Telegraf("7388313713:AAHaXOF3J9Fwq3dFuAEhNCgRQHoBHGQZLiA");
-const web_link = "https://michael-folio.vercel.app/";
-// console.log("Bot Token:", '7388313713:AAHaXOF3J9Fwq3dFuAEhNCgRQHoBHGQZLiA');
+const bot = new Telegraf("7017113574:AAGmdQwaf4lNyq67i4BSg23-5M-E9VflAJM");
+const web_link = "https://solpages-landing-page.vercel.app/";
 
 bot.start((ctx) =>
-  ctx.reply("Welcome, This is from Micah!. Click on the buton to start bot", {
-    reply_markup: {
-      keyboard: [
-        [
-          {
-            text: "web app",
-            web_app: {
-              url: web_link,
+  ctx.reply(
+    "Welcome, This is from Micah! Click the button below to start the bot.",
+    {
+      reply_markup: {
+        inline_keyboard: [
+          [
+            {
+              text: "Open Web App",
+              web_app: {
+                url: web_link,
+              },
             },
-          },
+          ],
         ],
-      ],
-    },
-  })
+      },
+    }
+  )
 );
 
 bot.launch();
